853e306a24726176c41a039b3ecebca0ea5a3cb6
patel-zeel <patel_zeel@iitgn.ac.in>
2022-01-16 00:32:56 +0500

    fix dependency

--- tests/test_gpytorch.py
+++ tests/test_gpytorch.py
@@ -3,11 +3,21 @@ import torch
 import gpytorch
 from regdata import Olympic, NonStat2D
 from gpytorch.kernels import RBFKernel, ScaleKernel
-from skgpytorch.models import ExactGPRegressor
-from skgpytorch.metrics import mean_squared_error
 
 from nsgptorch.models import GP
 from nsgptorch.kernels import rbf
+
+
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood, kernel):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = kernel
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
 
 
 @pytest.mark.eq2d
@@ -22,29 +32,42 @@ def test_equals_2d():
     our_kernel = [rbf for _ in range(X_train.shape[1])]
     our_inducing = [None for _ in range(X_train.shape[1])]
 
-    model = ExactGPRegressor(X_train, y_train, kernel)
+    likelihood = gpytorch.likelihoods.GaussianLikelihood()
+    model = ExactGPModel(X_train, y_train, likelihood, kernel)
+    mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
     our_model = GP(our_kernel, X_train.shape[1], our_inducing)
 
-    optimizer = torch.optim.Adam(our_model.parameters(), lr=0.1)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.1)
+    our_optimizer = torch.optim.Adam(our_model.parameters(), lr=0.1)
 
-    for seed in range(10, 15):
-        model.fit(n_iters=n_iters, random_state=seed)
+    for seed in range(5):
 
+        torch.manual_seed(seed)
+        for param in model.parameters():
+            torch.nn.init.normal_(param)
         torch.manual_seed(seed)
         for param in our_model.parameters():
             torch.nn.init.normal_(param)
 
         for _ in range(n_iters):
             optimizer.zero_grad()
-            loss = our_model(X_train, y_train)
+            our_optimizer.zero_grad()
+            out = model(X_train)
+            loss = -mll(out, y_train)
+            our_loss = our_model(X_train, y_train)
             loss.backward()
+            our_loss.backward()
             optimizer.step()
+            our_optimizer.step()
 
-        dist = model.predict(X_train, y_train, X_test)
+        model.eval()
+        with torch.no_grad(), gpytorch.settings.fast_pred_var():
+            dist = likelihood(model(X_test))
+
         our_dist = our_model.predict(X_train, y_train, X_test)
 
-        mse = mean_squared_error(dist, y_test, squared=False)
-        our_mse = mean_squared_error(our_dist, y_test, squared=False)
+        mse = (dist.mean.ravel() - y_test.ravel()).pow_(2).mean().sqrt_()
+        our_mse = (our_dist.mean.ravel() - y_test.ravel()).pow_(2).mean().sqrt_()
 
-        assert abs(mse - our_mse) < 0.01
+        assert abs(mse - our_mse) < 0.05
         # print(seed, "our", our_mse, "their", mse, "diff", abs(mse - our_mse))
